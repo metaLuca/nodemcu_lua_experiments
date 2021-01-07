cb878cff0ba253abce15467ff269675374c008ac
Marcato Luca <marcato@intre.it>
2021-01-07 01:01:02 +0100

    first commit

---
+++ bluetooth.lua
@@ -0,0 +1,17 @@
+su = softuart.setup(9600, 2, 3)
+
+function onReceive(data)
+  local txt = string.gsub(data, "[\r\n]", "")
+  if(txt == "1") then
+    su:write("1 on\n")
+  elseif(txt == "2") then
+    su:write("2 off\n")
+  else
+    su:write("--> " .. txt .. "\n")
+  end
+end
+
+function openBluetooth2()
+  su:on("data", "\n", onReceive)
+  su:write("-- bluetooth START --\n")
+end
---
+++ init.lua
@@ -0,0 +1,21 @@
+function startup()
+  print("startup!!!")
+
+-- while true than do
+  dofile("mpu.lua")
+  dofile("bluetooth.lua")
+  openBluetooth2()
+-- tmr.delay(100000)   -- 100ms timer delay
+-- end
+  if file.open("lucapilot.lua") == nil then
+    print("lucapilot.lua missing")
+  else
+    print("Running lucapilot.lua")
+    file.close("lucapilot.lua")
+--    open main
+  end
+end
+
+print("Startup will resume momentarily, you have 5 seconds to abort.")
+print("Waiting...")
+tmr.create():alarm(5000, tmr.ALARM_SINGLE, startup)
